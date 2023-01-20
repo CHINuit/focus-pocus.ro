fdebd956cb3ad70ad1383e7679ebc5b8daa88da1
chinuit <chinuit93@gmail.com>
2023-01-21 01:07:46 +0200

    a

---
+++ build/sync.sh
@@ -0,0 +1,4 @@
+cd /var/www/
+sudo git clone https://github.com/CHINuit/focus-pocus.ro.git
+cd /var/www/focus-pocus.ro/
+sudo git pull https://github.com/CHINuit/focus-pocus.ro.git
--- src/Webhook.jsx
+++ src/Webhook.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Webhook extends React.Component {
     handleWebhook() {
         // Send a request to the endpoint on the local machine
-        fetch('http://your-local-ip:3000/webhook', { method: 'POST' })
+        fetch('http://focus-pocus.ro/webhook', { method: 'POST' })
             .then(res => res.json())
             .then(data => console.log(data))
             .catch(error => console.error(error));
