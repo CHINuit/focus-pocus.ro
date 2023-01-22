d6d39270547fa5149738b6f6421b2a5dcaafe919
chinuit <chinuit93@gmail.com>
2023-01-22 02:52:22 +0200

    a

--- src/Components/Container.js
+++ src/Components/Container.js
@@ -6,15 +6,15 @@ const Container = () => {
         <div className="container">
             <div className="video-background">
                 <video autoPlay loop muted>
-                    <source src="/public/main.mp4" type="video/mp4" />
-                    <source src="/public/main.mp4" type="video/webm" />
+                    <source src="/main.mp4" type="video/mp4" />
+                    <source src="/main.mp4" type="video/webm" />
                 </video>
             </div>
             <div className="content">
                 {
                     <div className='flex'>
-                        <a className="link" href="https://artipic.net"><h1 className="link">Artipic</h1></a>
-                        <a className="linkf" href="https://www.facebook.com/FoCuS.s.PoCuS.s/"><h1 className="linkf">FoCuS PoCuS</h1></a>
+                        <a className="yellow" href="https://artipic.net"><h1 className="yellow">Artipic</h1></a>
+                        <a className="red" href="https://www.facebook.com/FoCuS.s.PoCuS.s/"><h1 className="red">FoCuS PoCuS</h1></a>
                     </div>}
             </div>
         </div>
