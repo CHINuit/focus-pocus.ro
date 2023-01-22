121e11ca155431ddd3ae59c68a2b59d16b5d4122
chinuit <chinuit93@gmail.com>
2023-01-22 02:30:50 +0200

    a

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ import './App.css';
 import Container from './Components/Container';
 
 function App() {
-
-  <div className='flex'>
-    <a className="link" href="https://artipic.net"><h1 className="link">Artipic</h1></a>
-    <a className="linkf" href="https://www.facebook.com/FoCuS.s.PoCuS.s/"><h1 className="linkf">FoCuS PoCuS</h1></a>
-  </div>
   return (
     <>
       <Container />
